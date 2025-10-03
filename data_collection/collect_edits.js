8eb13c5358f5f7be8b15f8b8d57dc685fdf58791
NalinPlad <43052612+NalinPlad@users.noreply.github.com>
2025-10-03 12:16:29 -0700

    adding url and props

--- data_collection/collect_edits.js
+++ data_collection/collect_edits.js
@@ -1,3 +1,15 @@
 import { DatabaseSync } from 'node:sqlite';
 const database = new DatabaseSync('./ucbwiki_data.db');
 
+const req_url = "https://en.wikipedia.org/w/api.php?origin=*"
+
+const params = {
+	"action": "query",
+	"format": "json",
+	"list": "usercontribs",
+	"formatversion": "2",
+	"uclimit": "max",
+	"uciprange": "2607:f140:400::/48",
+	"ucprop": "ids|title|timestamp|comment|size|flags|tags|sizediff|oresscores"
+}
+
